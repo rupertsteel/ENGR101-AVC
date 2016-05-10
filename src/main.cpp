fba2b6cb8329b92aeb6c4be7d305ca6d53348cca
Rupert Steel <rupert@rupertsteel.me>
2016-05-10 12:44:36 +1200

    Made a basic line following algorithm. This may fail.

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,27 @@ int main(int argc, char* argv[]) {
 	init(1);
 	
 	//openGate(); // the gate server isn't up yet, so this will block
-	moveForward();
+	//moveForward();
+	
+	pictureAnalysisData data;
+	data.last_error = 0.0f;
+	data.total_error = 0.0f;
+	
+	float turnResponse = 1;
+	
+	while (true) {
+		float signal = analysePicture(data);
+		
+		if (signal < -0.1f) {
+			turnLeftRadius(signal * turnResponse)
+		} else if (signal > 0.1f) {
+			turnRightRadius(signal * turnResponse);
+		} else {
+			moveForward();
+		}
+		
+		Sleep(0, 11111);
+	}
 	
 	return 0;
 }
