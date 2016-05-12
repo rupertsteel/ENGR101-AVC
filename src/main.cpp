5d044a95157814f5493b9b4c6580d5b69f411150
Rupert Steel <rupert@rupertsteel.me>
2016-05-13 11:39:21 +1200

    Added a signal hander to stop the motors with ^C

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
 
 #include "libE101.h"
 
+#include <signal.h>
+
+void handle_signal(int signal) {
+	if (signal == SIGINT) {
+		set_motor(1, 0);
+		set_motor(2, 0);
+	}
+}
+
 int main(int argc, char* argv[]) {
 	init(1);
+	
+	// set a signal handler to stop the motors when we exit the program with ^C
+	// code from https://gist.github.com/aspyct/3462238
+	sigaction sa;
+	sa.sa_handler = &handle_signal;
+	sa.sa_flags = SA_RESTART;
+	
+	sigfillset(&sa.sa_mask);
+	sigaction(SIGINT, &sa, NULL);
 	
 	//openGate(); // the gate server isn't up yet, so this will block
 	//moveForward();
