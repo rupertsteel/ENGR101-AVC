4f41403bc611145065fa1dcc8b80745ba330a548
Rupert Steel <rupert@rupertsteel.me>
2016-05-19 13:43:25 +1200

    Updated gate server port

--- src/open-gate.cpp
+++ src/open-gate.cpp
@@ -3,7 +3,7 @@
 
 void openGate() {
 	//connects to server with the ip address 192.168.1.2
-    connect_to_server("130.195.6.196", 22);
+    connect_to_server("130.195.6.196", 1024);
     //sends a message to the connected server
     send_to_server("Hello server");
     //receives message from the connected server
