8777ae749d9cdf72a4c577bd26dd34552397a652
Rupert Steel <rupert@rupertsteel.me>
2016-05-19 13:44:42 +1200

    Updated the message to the server.

--- src/open-gate.cpp
+++ src/open-gate.cpp
@@ -5,7 +5,7 @@ void openGate() {
 	//connects to server with the ip address 192.168.1.2
     connect_to_server("130.195.6.196", 1024);
     //sends a message to the connected server
-    send_to_server("Hello server");
+    send_to_server("Please");
     //receives message from the connected server
     char message[24];
     receive_from_server(message);
