bc65008b2ce77f467b729c7462c3560be045bd64
harshita-bandi <bandi.harshita@gmail.com>
2016-05-26 12:53:34 +1200

    added IR sensor code

--- src/IRsensor.cpp
+++ src/IRsensor.cpp
@@ -1,5 +1,14 @@
 #include "libE101.h"
 
 float IRsensor() {
-
-}
+ //read IR sensors
+ front = ReadAnalog (0);
+ right= ReadAnalog (1);
+ left = ReadAnalog (2);
+float signal =0;
+ //if  right reading is more than the left reading we are gonna turn right
+ (right>left){
+   
+ }
+ if(left>right){
+}if
